ecaafd7fad488e07e09f1842a1522d8024b17af9
SJ-Hong-hash <seojoon.hong94@gmail.com>
2020-09-26 02:51:55 +0900

    model 객체 만들기 done

--- api/validators.py
+++ api/validators.py
@@ -1,7 +1,8 @@
 from django.core.exceptions import ValidationError
 
+
 def validate_score(value):
     """평점(score)이 10보다 크면 Validation Error 를 일으킨다."""
-    if (value > 10) | (value < 0 ):
+    if (value > 10) | (value < 0):
         msg = u"'평점은 0 이상 10 이하로 매겨주세요."
         raise ValidationError(msg)
