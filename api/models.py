06f2ae35a7a43e33b47e40087fce6761ebb80b7a
fulladdr <jeymemoon@gmail.com>
2020-09-26 09:45:50 +0900

    posts model made

--- api/models.py
+++ api/models.py
@@ -1,8 +1,27 @@
+from django.contrib.auth.models import AbstractUser
+from django.db.models import CharField
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
 
-# 모델링 과제를 이곳에서 해주시면 됩니다! (주석은 나중에 지우셔도 돼요!)
 
-# [제약조건]
-# 1. 1:1과 1:n의 관계 포함
-# 2. 각 모델에 필드 최소 3개 이상 포함
-# 3. 서비스 관련 모델 3개 이상 + 유저 모델 1개 구현 (단, 유저는 필수 아님)
+class User(AbstractUser):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('C', 'Custom'),
+    ]
+
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    user_name = models.CharField(blank=True, max_length=255)
+    profile_photo = models.ImageField(blank=True)
+    website = models.URLField(blank=True)
+    bio = models.TextField(blank=True)
+    email = models.CharField(blank=True, max_length=255)
+    phone_number = models.CharField(blank=True, max_length=255)
+    gender = models.CharField(blank=True, choices=GENDER_CHOICES, max_length=255)
+    followers = models.ManyToManyField("self")
+    following = models.ManyToManyField("self")
+
+    def get_absolute_url(self):
+        return reverse("users:detail", kwargs={"username": self.username})
