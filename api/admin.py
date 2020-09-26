51ddd09880ebbb77b38b9aeb72ebc19afdcc15ee
somsomdah <somdah98@gmail.com>
2020-09-26 19:45:02 +0900

    add user models

--- api/admin.py
+++ api/admin.py
@@ -4,8 +4,33 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import *
 
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = (
+        'student_code',
+        'student_name',
+        'student_department'
+    )
+
+    list_display_links = (
+        'student_code',
+    )
+
+
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = (
+        'professor_code',
+        'professor_name',
+        'professor_department'
+    )
+
+    list_display_links = (
+        'professor_code',
+    )
+
+
 admin.site.register(Contact)
-admin.site.register(Student)
-admin.site.register(Professor)
+admin.site.register(Student,StudentAdmin)
+admin.site.register(Professor,ProfessorAdmin)
 admin.site.register(Course)
 admin.site.register(Enrollment)
--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
 # 모델링 과제를 이곳에서 해주시면 됩니다! (주석은 나중에 지우셔도 돼요!)
@@ -6,35 +7,66 @@ from django.db import models
 # [제약조건]
 # 1. 1:1과 1:n의 관계 포함
 # 2. 각 모델에 필드 최소 3개 이상 포함
-# 3. 서비스 관련 모델 3개 이상 + 유저 모델 1개 구현 (단, 유저는 필수 아님) # 서비스모델?? 유저모델???
+# 3. 서비스 관련 모델 3개 이상 + 유저 모델 1개 구현 (단, 유저는 필수 아님)
+
+class StudentManager(BaseUserManager):
+    use_in_migrations = True
+
+    def student_user(self, code, name, password=None):
+        if not code:
+            raise ValueError('must have user id')
+        if not name:
+            raise ValueError('must have user name')
+
+        user = self.model(code=code, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class ProfessorManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_professor_user(self, code, name, password=None):
+        if not code:
+            raise ValueError('must have user id')
+        if not name:
+            raise ValueError('must have user name')
+
+        user = self.model(code=code, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
 class Contact(models.Model):  # 소속
-    email=models.CharField(max_length=40,null=True)
-    phone_number=models.CharField(max_length=20,null=True)
-    office_number=models.CharField(max_length=20,null=True)
+    email = models.EmailField(max_length=40, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    office_number = models.CharField(max_length=20, null=True, blank=True)
 
 
 class Student(models.Model):  # 학생
-    student_name = models.CharField(max_length=20)  # 학생이름
-    student_department = models.CharField(max_length=20,null=True)  # 소속(전공), 모든 학생 전공이 1개임을 가정
-    student_id = models.CharField(max_length=20)  # 학번 != Pk
-    contact=models.ForeignKey(Contact,on_delete=models.SET_NULL,null=True)
+    objects = StudentManager()
+    student_code = models.CharField(max_length=20,verbose_name='학번')
+    student_name = models.CharField(max_length=20,verbose_name='이름')
+    student_department = models.CharField(max_length=20, null=True, blank=True,verbose_name='소속')
+    contact = models.OneToOneField(Contact, on_delete=models.SET_NULL, null=True, blank=True)
 
 
 class Professor(models.Model):  # 교수
-    professor_name = models.CharField(max_length=20)  # 교수 이름
-    professor_department = models.CharField(max_length=20,null=True)  # 교수 소속
-    professor_major = models.CharField(max_length=40)  # 교수 세부 전공
-    contact = models.ForeignKey(Contact, on_delete=models.SET_NULL,null=True)
+    objects = ProfessorManager()
+    professor_code = models.CharField(max_length=20,verbose_name='교수번호')
+    professor_name = models.CharField(max_length=20,verbose_name='이름')
+    professor_department = models.CharField(max_length=20, null=True, blank=True,verbose_name='소속')
+    professor_major = models.CharField(max_length=40, null=True, blank=True)  # 교수 세부 전공
+    contact = models.OneToOneField(Contact, on_delete=models.SET_NULL, null=True, blank=True)
 
 
 class Course(models.Model):  # 강좌
-    professor = models.ForeignKey(Professor, on_delete=models.SET_NULL,null=True)  # 교수
-    course_name = models.CharField(max_length=40)  # 강좌명
-    course_id = models.CharField(max_length=20)  # 학수번호!=PK
-    classroom = models.CharField(max_length=20)  # 강의실
+    professor = models.ForeignKey(Professor, on_delete=models.SET_NULL, null=True, blank=True)
+    course_name = models.CharField(max_length=40)
+    course_code = models.CharField(max_length=20)
+    classroom = models.CharField(max_length=20, null=True)
 
 
 class Enrollment(models.Model):  # 수강신청, 관계테이블
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)  # 학생
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)  # 강좌
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
