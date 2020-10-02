cf9c3bf6c61382359ac7361962182e5ffa4b5a5a
Tiffany Lee <tiffany@Tiffanyui-MacBook-Pro.local>
2020-10-02 14:39:39 +0900

    modify model

--- api/models.py
+++ api/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-# 모델링 과제를 이곳에서 해주시면 됩니다! (주석은 나중에 지우셔도 돼요!)
-
-# [제약조건]
-# 1. 1:1과 1:n의 관계 포함
-# 2. 각 모델에 필드 최소 3개 이상 포함
-# 3. 서비스 관련 모델 3개 이상 + 유저 모델 1개 구현 (단, 유저는 필수 아님)
 
 class Profile(models.Model):
     age = models.IntegerField()
@@ -24,17 +18,19 @@ class Movie(models.Model):
 
 class Timetable(models.Model):
     start_time = models.DateTimeField()
-    seat_left = models.IntegerField(default=200)
-    movie = models.ForeignKey("Movie", on_delete=models.CASCADE, related_name="movie_play_info")
-    #일단 둘 다 놔두었습니다-관련 질문이 있어서요!
-    profile = models.ManyToManyField(Profile, blank=True)
+    movie = models.ForeignKey("Movie", on_delete=models.CASCADE, related_name="movie_play_info"),
     user = models.ManyToManyField(User, blank=True)
+
+    def count_spare_seat(self):
+        MAX_SEATS = 200
+        return MAX_SEATS - self.user.all().count()
 
 class Comment(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     create_time = models.DateTimeField(auto_now_add=True)
     movie = models.ForeignKey("Movie", on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="user_comment")
 
 
 
