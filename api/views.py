c927d70453d833d06782643a63ce596759eaf8a8
Tiffany Lee <tiffany@Tiffanyui-MacBook-Pro.local>
2020-10-03 16:37:07 +0900

    add views

--- api/views.py
+++ api/views.py
@@ -1,5 +1,48 @@
 from django.shortcuts import render
+from .serializers import MovieSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Movie
+from django.http import Http404
 
-# Create your views here.
+# 1. read all movie
+class MovieList(APIView):
+    def get(self, request, format=None):
+        queryset = Movie.objects.all()
+        serializer = MovieSerializer(queryset, many=True)
+        json_data = serializer.data
+        return Response(json_data)
 
+    def post(self, request, format=None):
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#serializer에서 request.date 만 받아오면 새로운 데이터를 저장한다는 것,
+#queryset(원래 있던 데이터)까지 받아오면 수정을 한다는 것
+
+
+class MovieDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            raise Http404
+
+
+    def put(self, request, pk, format=None):
+        queryset = self.get_object(pk)
+        serializer = MovieSerializer(queryset, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        queryset = self.get_object(pk)
+        queryset.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
