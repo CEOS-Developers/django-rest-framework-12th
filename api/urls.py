35fbea3152733eb7b489987e8783d308040ec03f
kkirru <qorgusdn04@gmail.com>
2020-10-30 00:59:14 +0900

    list_created_today action added

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,13 @@ from . import views
 from rest_framework import routers
 
 # CBV url patterns
-urlpatterns = [
-    path('routine/', views.RoutineList.as_view()),
-    path('routine/<int:pk>/', views.RoutineDetail.as_view())
-]
+# urlpatterns = [
+#     path('routine/', views.RoutineList.as_view()),
+#     path('routine/<int:pk>/', views.RoutineDetail.as_view())
+# ]
 
+# Viewset url patterns
+router = routers.DefaultRouter()
+router.register(r'routine', views.RoutineViewSet)
+
+urlpatterns = router.urls
