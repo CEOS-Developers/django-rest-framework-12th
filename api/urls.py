c2179cec6f0f37945d6fa149be107271409e3961
Taeeun Kim <tena.kim10@gmail.com>
2020-11-08 07:50:36 +0900

    Revert "[12기_이은솔] DRF1: APIView 미션 제출합니다."

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('movie/', views.MovieList.as_view()),
-    path('movie/<int:pk>/', views.MovieDetail.as_view()),
-    path('comment/', views.CommentList.as_view()),
-    path('comment/<int:pk>/', views.CommentDetail.as_view()),
-]
